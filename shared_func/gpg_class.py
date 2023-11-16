1daeded26eda8d5477053d3e8f9f12053d8752be
s33ding <robertomdiniz@protonmail.com>
2023-11-16 13:43:53 -0300

    saving

--- shared_func/gpg_class.py
+++ shared_func/gpg_class.py
@@ -1,26 +1,61 @@
 import os
+import sys
 
 class GPG:
-    def __init__(self, recipient_email):
-        self.recipient_email = recipient_email
+    def __init__(self, file_name=None):
 
-    def encrypt_file(self, input_file):
+        self.recipient_email = os.environ.get("GPG_RECIPIENT_EMAIL")
+
+        if self.recipient_email is None:
+            print("GPG_RECIPIENT_EMAIL not found in environment variables.")
+            return False 
+        else:
+            self.get_file(file_name)
+            file_name = self.file_name
+            print("processing:", file_name)
+            self.process(file_name)
+
+
+    def get_file(self, file_name): 
+        if file_name is None:
+            if len(sys.argv) > 1:
+                file_name = sys.argv[1]
+            else:
+                file_name = input("input_file:",)
+
+        self.file_name = file_name
+
+    def encrypt_file(self, file_name):
+        self.file_name = file_name
+        self.output_file = f"{self.file_name}.gpg"
+
         # Encrypt the file
-        command = f'gpg -e -r {self.recipient_email} {input_file}'
-        print(command)
+        command = f'gpg -e -r {self.recipient_email} {self.file_name}'
+        print("cmd:",command)
         os.system(command)
 
         # Delete the original file after encryption
-        print("rmv:",input_file)
-        os.remove(input_file)
+        print("rmv:",self.file_name)
+        os.remove(self.file_name)
 
-    def decrypt_file(self, input_file_gpg):
+    def decrypt_file(self, file_name):
+        self.file_name = file_name
+        self.output_file = file_name.replace(".gpg","")
+
         # Decrypt the file
-        output_file = input_file_gpg.replace(".gpg","")
-        command = f'gpg -d {input_file_gpg} > {output_file}'
-        print(command)
+        command = f'gpg -d {self.file_name} > {self.output_file}'
+        print("cmd:",command)
         os.system(command)
 
         # Delete the encrypted file after decryption
-        print("rmv:",input_file_gpg)
-        os.remove(input_file_gpg)
+        print("rmv:",self.file_name)
+        os.remove(self.file_name)
+
+    def process(self, file_name):
+        if file_name.endswith(".gpg"):
+            # Decrypt a file and delete the original
+            self.decrypt_file(file_name)
+        else:
+            # Encrypt a file and delete the original
+            self.encrypt_file(file_name)
+
