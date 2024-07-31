3ac43f203f0920cbe6a696601b92f14f6117364b
s33ding <robertomdiniz@protonmail.com>
2024-07-31 10:46:51 -0300

    saving

--- pandas_query_mysql.py
+++ pandas_query_mysql.py
@@ -9,8 +9,11 @@ pth = f"{os.environ["CRYPTOGRAPHY"]}/shared_func"
 sys.path.append(pth)
 
 from mysql_class import MySQL
+from boto3_class import Boto
 
 db = MySQL()
+b3 = Boto()
+db.cred = b3.get_secret(os.environ["MYSQL_READ_SECRET"])
 print("------------------------")
 #input: query, the default value for query is None
 df = db.qry()
